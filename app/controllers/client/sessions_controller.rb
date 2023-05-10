898bf9af086d97536f38b260469e0cccd2073334
calrmichealacot <calrmichealacot@gmail.com>
2023-05-10 20:02:25 +0800

    adding CLient Domain and COntrollers

---
+++ app/controllers/client/sessions_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class Client::SessionsController < Devise::SessionsController
+   before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+   def new
+     super
+   end
+
+  # POST /resource/sign_in
+   def create
+     @user = User.find_by_email(params[:user][:email])
+     if @user&.admin? && @user&.valid_password?(params[:user][:password])
+       flash[:alert] = "You don't have access to this page"
+       redirect_to client_user_session_path
+     else
+       super
+     end
+   end
+
+  # DELETE /resource/sign_out
+   def destroy
+     super
+   end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  #def configure_sign_in_params
+  #  devise_parameter_sanitizer.permit(:sign_in, keys: [:email][:password])
+  #end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  root 'admin/home#index'
+  constraints(AdminDomainConstraint.new) do
+    devise_for :users, as: 'admin', controllers: { sessions: 'admin/sessions'}
+    namespace :admin do
+      resources :users
+    end
+  end
 
-   constraints(AdminDomainConstraint.new) do
-       namespace :admin do
-         resources :users
-       end
-   end
+  constraints(ClientDomainConstraint.new) do
+    devise_for :users, as: 'client', controllers: { sessions: 'client/sessions', registrations: 'client/registrations' }
+    resources :posts do
+    end
+  end
+
+  root 'admin/home#index'
 end
 
