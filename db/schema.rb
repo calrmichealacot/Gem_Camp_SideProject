ea7f93e4f784a9c5b4511d6a02e5de1bf69179d1
calrmichealacot <calrmichealacot@gmail.com>
2023-05-14 19:30:24 +0800

    add table for username and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_14_105628) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_112820) do
   create_table "address_barangays", charset: "utf8mb4", force: :cascade do |t|
     t.bigint "city_id"
     t.string "code"
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_14_105628) do
     t.integer "role", default: 0
     t.string "image"
     t.string "phone_number"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
