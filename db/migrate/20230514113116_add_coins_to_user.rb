6cd124079860ecfb207350091e627444f3981a5b
calrmichealacot <calrmichealacot@gmail.com>
2023-05-14 19:46:10 +0800

    roll back for add_coins_to_user

--- db/migrate/20230514113116_add_coins_to_user.rb
+++ db/migrate/20230514113116_add_coins_to_user.rb
@@ -1,5 +1,5 @@
 class AddCoinsToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :coins, :integer
+    add_column :users, :coins,:decimal ,precision: 18, scale: 2, default: "0.0"
   end
 end
