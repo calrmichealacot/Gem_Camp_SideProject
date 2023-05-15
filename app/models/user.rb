6d5c1f850c020f79b3fa7bf0ae316c93087289b0
calrmichealacot <calrmichealacot@gmail.com>
2023-05-15 21:59:17 +0800

    user increment, childrens member

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :phone_number, uniqueness: true
+  validates :email, uniqueness:  true
+  belongs_to :parent, class_name: 'User', counter_cache: :children_members, optional: true
+  has_many :children, class_name: 'User', foreign_key: :parent_id
+  has_many :user_address
   enum genre: { client: 0, admin: 1 }
   belongs_to :user
   mount_uploader :image, ImageUploader
