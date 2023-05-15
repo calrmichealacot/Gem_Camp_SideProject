f8b4bed6d31e5219afb71472f052505aee8c7119
calrmichealacot <calrmichealacot@gmail.com>
2023-05-15 23:27:39 +0800

    creating ITEM MODEL

---
+++ db/migrate/20230515152108_item_category.rb
@@ -0,0 +1,9 @@
+class ItemCategory < ActiveRecord::Migration[7.0]
+  def change
+    create_table :item_category_ships do |t|
+      t.references :item
+      t.references :category
+      t.timestamps
+    end
+  end
+end
