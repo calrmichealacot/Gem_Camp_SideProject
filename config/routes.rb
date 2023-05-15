69a2676c2a20a27dba3f67994d999df86d92582b
calrmichealacot <calrmichealacot@gmail.com>
2023-05-15 23:45:56 +0800

    adding routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,35 @@ Rails.application.routes.draw do
     devise_for :users, as: 'admin', controllers: { sessions: 'admin/sessions' }
     namespace :admin do
       resources :users
-    end
-  end
+      resources :userlist
+      resources :item do
 
-  constraints(ClientDomainConstraint.new) do
-    devise_for :users, as: 'client', controllers: { sessions: 'client/sessions', registrations: 'client/registrations' }
-    namespace :client,path: '' do
-      resources :profiles, only:[:show, :edit, :update]
-      resources :user_address
-      resources :lottery
-      resources :invite
+      end
+    end
+
+    constraints(ClientDomainConstraint.new) do
+      devise_for :users, as: 'client', controllers: { sessions: 'client/sessions', registrations: 'client/registrations' }
+      namespace :client, path: '' do
+        resources :profiles, only: [:show, :edit, :update]
+        resources :user_address, path: 'address'
+        resources :lottery
+        resources :invite
+      end
     end
   end
 end
 
-
-
-
+namespace :api do
+  namespace :v1 do
+    resources :regions, only: %i[index show], defaults: { format: :json } do
+      resources :provinces, only: :index, defaults: { format: :json }
+    end
+    resources :provinces, only: %i[index show], defaults: { format: :json } do
+      resources :cities, only: :index, defaults: { format: :json }
+    end
+    resources :cities, only: %i[index show], defaults: { format: :json } do
+      resources :barangays, only: :index, defaults: { format: :json }
+    end
+    resources :barangays, only: %i[index show], defaults: { format: :json }
+  end
+end
